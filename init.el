ceef52c0c3f627c7748a3c4d7b941478c520a5f8
Sune Nordby <snordby@users.noreply.github.com>
2017-03-12 19:55:00 +0100

    Update init.el

--- init.el
+++ init.el
@@ -1,7 +1,12 @@
-;; Automatically added
-;;
 
 ;; Startup
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (setq user-full-name "Sune Nordby")
 (setq user-mail-adress "nordby@protonmail.com")
 (setq inhibit-startup-message t)
@@ -15,6 +20,12 @@
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
 (global-hl-line-mode 1)
+(global-linum-mode t)
+(add-to-list 'default-frame-alist '(font . "Inconsolata-13" ))
+(set-face-attribute 'default t :font "Inconsolata-13" )
+
+;; Theme
+(require 'color-theme-sanityinc-tomorrow)
 
 ;; Behavior
 (delete-selection-mode t)
@@ -22,22 +33,52 @@
 (setq x-select-enable-clipboard t)
 (show-paren-mode t)
 
-;; Packages
-(require 'package)
+;; Repositories
 (require 'package)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'default-frame-alist '(font . "Noto Mono-11" ))
-(set-face-attribute 'default t :font "Noto Mono-11" )
 
-(package-initialize)
+(defvar gnu '("gnu" . "https://elpa.gnu.org/packages/"))
+(defvar melpa '("melpa" . "https://melpa.org/packages/"))
+(defvar melpa-stable '("melpa-stable" . "https://stable.melpa.org/packages/"))
+
+;; Add marmalade to package repos
+(setq package-archives nil)
+(add-to-list 'package-archives melpa-stable t)
+(add-to-list 'package-archives melpa t)
+(add-to-list 'package-archives gnu t)
+
+(defun packages-install (&rest packages)
+  (message "running packages-install")
+  (mapc (lambda (package)
+          (let ((name (car package))
+                (repo (cdr package)))
+            (when (not (package-installed-p name))
+              (let ((package-archives (list repo)))
+                (package-initialize)
+                (package-install name)))))
+        packages)
+  (package-initialize)
+  (delete-other-windows))
 
 ;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
         (package-refresh-contents)
         (package-install 'use-package))
+
+;; Packages
+
+(use-package spaceline
+    :ensure t
+    :init
+    (setq powerline-default-separator 'utf-8)
+
+    :config
+    (require 'spaceline-config)
+    (spaceline-spacemacs-theme)
+    )
 
 (use-package try 
   :ensure t)
@@ -69,27 +110,24 @@
 (with-eval-after-load 'org
   (setq org-startup-indented t) ; Enable 'org-indent-mode' by default
   (add-hook 'org-mode-hook #'visual-line-mode))
-(require 'org-gcal)
-
-;; Org-agenda
 (global-set-key (kbd "C-c a") 'org-agenda)
 (setq org-agenda-show-log t
       org-agenda-todo-ignore-scheduled t
       org-agenda-todo-ignore-deadlines t)
 
-;; Custom set stuff
+;; Automatically added
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (hc-zenburn)))
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
  '(custom-safe-themes
    (quote
-    ("bcc6775934c9adf5f3bd1f428326ce0dcd34d743a92df48c128e6438b815b44f" default)))
+    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default)))
  '(package-selected-packages
    (quote
-    (undo-tree magit highlight-current-line evil twittering-mode wc-mode org-gcal pcmpl-git git-command org-beautify-theme htmlize json-mode jq-mode jquery-doc ace-window zenburn-theme which-key use-package try org-bullets hc-zenburn-theme 2048-game))))
+    (smart-yank color-theme-sanityinc-tomorrow org-bullets ace-window which-key try use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
